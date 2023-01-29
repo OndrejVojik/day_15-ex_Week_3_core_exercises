cc6d924aff6666b10ca0d9427b88d4b9d21b97c6
OndrejVojik <vojik.o@hotmail.com>
2023-01-29 10:18:13 +0100

    adding btn and clearing fields after adding

--- script.js
+++ script.js
@@ -83,4 +83,27 @@ const displayItems = (obj) => {
   });
 };
 
+const btn_add = document.querySelector(".add-item");
+btn_add.addEventListener("click", (e) => {
+  let item_name = document.querySelector("input[name='name']").value;
+  // const item_name = document.querySelector(".item-name-input").value;
+  let item_amount = document.querySelector("input[name='amount']").value;
+  let item_price = document.querySelector("input[name='price']").value;
+
+  shopping_cart.items.push({
+    name: item_name,
+    amount: item_amount,
+    price: item_price,
+  });
+  displayItems(shopping_cart);
+// this is not working
+//   item_name = "";
+//   item_amount = "";
+//   item_price = "";
+  document.querySelector("input[name='name']").value = "";
+  document.querySelector("input[name='amount']").value = "";
+  document.querySelector("input[name='price']").value = "";
+
+});
+
 displayItems(shopping_cart);
